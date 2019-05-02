e73775c499b573657a5d20c33f1b262ab7779ed8
Rodrigo Vanhoz Ribeiro <rodrigo.vanhoz@gmail.com>
2019-05-01 21:49:41 -0300

    Criando e tratando excessões

--- src/boardgame/Tabuleiro.java
+++ src/boardgame/Tabuleiro.java
@@ -7,6 +7,9 @@ public class Tabuleiro {
 	private Peca[][] pecas;
 	
 	public Tabuleiro(int linhas, int colunas) {
+		if (linhas < 1 || colunas < 1) {
+			throw new XadrezException("Erro ao criar o tabuleiro. Deverá ser criado pelo menos com tamanho de 1 x 1");
+		}
 		this.linhas = linhas;
 		this.colunas = colunas;
 		
@@ -17,28 +20,44 @@ public class Tabuleiro {
 		return linhas;
 	}
 
-	public void setLinhas(int linhas) {
-		this.linhas = linhas;
-	}
-
 	public int getColunas() {
 		return colunas;
 	}
 
-	public void setColunas(int colunas) {
-		this.colunas = colunas;
-	}
-	
 	public Peca peca(int linha, int coluna) {
+		if (!posicaoExists(linha, coluna)) {
+			throw new XadrezException("Posição fora do tabuleiro");
+		}
 		return pecas[linha][coluna];
 	}
 	
 	public Peca peca(Posicao posicao) {
+		if (!posicaoExists(posicao)) {
+			throw new XadrezException("Posição fora do tabuleiro");
+		}
 		return pecas[posicao.getLinha()][posicao.getColuna()];
 	}
 	
 	public void PosicaoPeca(Peca peca, Posicao posicao) {
+		if (isPeca(posicao)) {
+			throw new XadrezException("Já existe uma peça na posição " + posicao);
+		}
 		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
 		peca.posicao = posicao;
+	}
+	
+	private boolean posicaoExists(int linha, int coluna) {
+		return linha >= 0 && linha <= linhas && coluna >= 0 && coluna <= colunas;
+	}
+	
+	public boolean posicaoExists(Posicao posicao) {
+		return posicaoExists(posicao.getLinha(), posicao.getColuna());
+	}
+	
+	public boolean isPeca(Posicao posicao) {
+		if (!posicaoExists(posicao)) {
+			throw new XadrezException("Posição fora do tabuleiro");
+		}
+		return peca(posicao) != null;	
 	}
 }
